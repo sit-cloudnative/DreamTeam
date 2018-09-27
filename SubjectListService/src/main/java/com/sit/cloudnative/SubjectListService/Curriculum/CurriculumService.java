9b1018134f924733149c3bf1d8e907a369e46ece
Tanapat128 <tanapat.pm@mail.kmutt.ac.th>
2018-09-27 17:33:03 +0700

    [Add] CurriculumService not done

---
+++ SubjectListService/src/main/java/com/sit/cloudnative/SubjectListService/Curriculum/CurriculumService.java
@@ -0,0 +1,22 @@
+package com.sit.cloudnative.SubjectListService.curriculum;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CurriculumService implements CurriculumServiceInterface {
+
+    @Override
+    public Curriculum getCurriculumById(long curriculumId) {
+        
+        return null;
+    }
+
+    @Override
+    public List<Curriculum> getAllCurriculum() {
+		return null;
+	}
+
+
+}
