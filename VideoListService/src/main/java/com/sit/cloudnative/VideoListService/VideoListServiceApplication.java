47f55ed7f8f9561899f393d646089028a815cd78
Tanapat128 <tanapat.pm@mail.kmutt.ac.th>
2018-09-27 15:35:06 +0700

    init Project SubjectListService & VideoListService

---
+++ VideoListService/src/main/java/com/sit/cloudnative/VideoListService/VideoListServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sit.cloudnative.VideoListService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoListServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoListServiceApplication.class, args);
+	}
+}
