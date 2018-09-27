286714faabb17fc522b1d9f8ed5fb8474ba1ad8d
Tanapat128 <tanapat.pm@mail.kmutt.ac.th>
2018-09-27 19:51:58 +0700

    [Add] CurriculumService V1 "เมธอด getCurriculumById"

--- SubjectListService/src/main/java/com/sit/cloudnative/SubjectListService/Curriculum/CurriculumService.java
+++ SubjectListService/src/main/java/com/sit/cloudnative/SubjectListService/Curriculum/CurriculumService.java
@@ -2,15 +2,21 @@ package com.sit.cloudnative.SubjectListService.curriculum;
 
 import java.util.List;
 
+import com.sit.cloudnative.SubjectListService.adapter.ProgramAdapter;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CurriculumService implements CurriculumServiceInterface {
 
+    @Autowired
+    ProgramAdapter programAdapter;
+
     @Override
     public Curriculum getCurriculumById(long curriculumId) {
-        
-        return null;
+        Curriculum curriculum = programAdapter.getProgramDetail(curriculumId);
+        return curriculum;
     }
 
     @Override
