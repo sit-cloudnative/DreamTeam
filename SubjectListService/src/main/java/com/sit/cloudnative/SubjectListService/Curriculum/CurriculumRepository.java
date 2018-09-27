d7199e7857ce2cc58e51873df5f79d25db2e0b99
Tanapat128 <tanapat.pm@mail.kmutt.ac.th>
2018-09-27 17:17:28 +0700

    [Delete] curriculumRepository

--- SubjectListService/src/main/java/com/sit/cloudnative/SubjectListService/Curriculum/CurriculumRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sit.cloudnative.SubjectListService.Curriculum;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CurriculumRepository extends JpaRepository<Curriculum, Long> {
-    
-}
