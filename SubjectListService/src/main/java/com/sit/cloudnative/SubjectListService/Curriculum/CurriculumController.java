e1f82b31ddec3a6c59664ef10585d2f6a1e9674c
Tanapat128 <tanapat.pm@mail.kmutt.ac.th>
2018-09-27 22:12:07 +0700

    [Add] add CurriculumController

--- SubjectListService/src/main/java/com/sit/cloudnative/SubjectListService/Curriculum/CurriculumController.java
+++ SubjectListService/src/main/java/com/sit/cloudnative/SubjectListService/Curriculum/CurriculumController.java
@@ -1,5 +1,7 @@
 package com.sit.cloudnative.SubjectListService.curriculum;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,5 +19,11 @@ public class CurriculumController{
     public ResponseEntity<Curriculum> getCurriculum(@PathVariable("curriculumId")long curriculumId){
         Curriculum curriculum = curriculumService.getCurriculumById(curriculumId);
         return new ResponseEntity<Curriculum>(curriculum, HttpStatus.OK);
+    }
+
+    @GetMapping(value="/curriculum")
+    public ResponseEntity<List<Curriculum>> getCurriculumList(){
+        List<Curriculum> curriculum = curriculumService.getAllCurriculum();
+        return new ResponseEntity<List<Curriculum>>(curriculum, HttpStatus.OK);
     }
 }
