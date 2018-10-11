08e2483ef7e5e9b307c458ae56d39c3f0be52a48
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-10-11 18:02:07 +0700

    [ADD] add history model

---
+++ HistoryService/src/main/java/com/sit/cloudnative/HistoryService/History.java
@@ -0,0 +1,67 @@
+package com.sit.cloudnative.HistoryService;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "histories")
+class History implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int historyId;
+
+    @NotBlank
+    private int videoId;
+
+    @NotBlank
+    private String videoName;
+    private String lecturer;
+    private String videoThumbnail;
+    private int checkpoint;
+
+    public int getHistoryId() {
+        return historyId;
+    }
+    public int getCheckpoint() {
+        return checkpoint;
+    }
+    public String getVideoThumbnail() {
+        return videoThumbnail;
+    }
+    public String getLecturer() {
+        return lecturer;
+    }
+    public String getVideoName() {
+        return videoName;
+    }
+    public int getVideoId() {
+        return videoId;
+    }
+    
+    public void setCheckpoint(int checkpoint) {
+        this.checkpoint = checkpoint;
+    }
+    public void setVideoThumbnail(String videoThumbnail) {
+        this.videoThumbnail = videoThumbnail;
+    }
+    public void setLecturer(String lecturer) {
+        this.lecturer = lecturer;
+    }
+    
+    public void setVideoName(String videoName) {
+        this.videoName = videoName;
+    }
+    public void setVideoId(int videoId) {
+        this.videoId = videoId;
+    }
+    public void setHistoryId(int historyId) {
+        this.historyId = historyId;
+    }
+}
