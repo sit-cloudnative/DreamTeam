cc3cb1350efe493745df0783b39bf2c3f69f36db
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2018-10-11 16:54:45 +0700

    init Project HistoryService

---
+++ HistoryService/src/test/java/com/sit/cloudnative/HistoryService/HistoryServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sit.cloudnative.HistoryService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HistoryServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
