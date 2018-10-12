03719615b6d0d3597d3aeb1b887b094f757d0d44
Tanapat128 <tanapat.pm@mail.kmutt.ac.th>
2018-10-12 22:38:48 +0700

    [Update] get first index of List<History>

--- HistoryService/src/main/java/com/sit/cloudnative/HistoryService/HistoryService.java
+++ HistoryService/src/main/java/com/sit/cloudnative/HistoryService/HistoryService.java
@@ -6,27 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class HistoryService{
+public class HistoryService {
     @Autowired
     private HistoryRepository historyRepository;
 
-    public History findById(long id){
+    public History findById(long id) {
         History history = historyRepository.getOne(id);
         return history;
     }
 
-    public long update(History history){
+    public long update(History history) {
         return historyRepository.save(history).getVideoId();
     }
 
-    public int getCheckpointInSecond(long studentId, long videoId){
+    public int getCheckpointInSecond(long studentId, long videoId) {
         int checkpointInSecond;
-        try{
-            History historyObject = historyRepository.findByStudentIdAndVideoId(studentId, videoId);
+        try {
+            List<History> historyList = historyRepository.findByStudentIdAndVideoId(studentId, videoId);
+            History historyObject = historyList.get(0);
             checkpointInSecond = historyObject.getCheckpoint();
-        }catch(NullPointerException e){
-            checkpointInSecond =  -1;
+        } catch (NullPointerException e) {
+            checkpointInSecond = -1;
         }
         return checkpointInSecond;
     }
+
 }
