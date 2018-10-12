62763cd09431d7ffab8f5fa446401aef49176031
Tanapat128 <tanapat.pm@mail.kmutt.ac.th>
2018-10-12 17:25:37 +0700

    [UPDATE] getCheckpoint in second method

--- HistoryService/src/main/java/com/sit/cloudnative/HistoryService/HistoryService.java
+++ HistoryService/src/main/java/com/sit/cloudnative/HistoryService/HistoryService.java
@@ -1,5 +1,7 @@
 package com.sit.cloudnative.HistoryService;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +17,15 @@ public class HistoryService{
 
     public long update(History history){
         return historyRepository.save(history).getVideoId();
+    }
+
+    public int getCheckpointInSecond(long studentId, long videoId){
+        System.out.print("***************Im here***************");
+        History historyObject = historyRepository.findByStudentIdAndVideoId(studentId, videoId);
+        System.out.print("**********************************************");
+        System.out.print(historyObject.getCheckpoint());
+        System.out.print("**********************************************");
+        int checkpointInSecond = historyObject.getCheckpoint();
+        return checkpointInSecond;
     }
 }
