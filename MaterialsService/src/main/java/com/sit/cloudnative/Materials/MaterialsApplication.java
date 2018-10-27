5c13bac49a3aed84cfcb28f2f4942960ced4d99c
Mee <wutthipong.mee@mail.kmutt.ac.th>
2018-10-27 15:42:07 +0700

    [ADD]MaterialService

---
+++ MaterialsService/src/main/java/com/sit/cloudnative/Materials/MaterialsApplication.java
@@ -0,0 +1,12 @@
+package com.sit.cloudnative.Materials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialsApplication.class, args);
+	}
+}
