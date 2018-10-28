29a07a61a5932a50165a06c201455a32aeda4c3d
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.th>
2018-10-28 18:34:36 +0700

    [ADD] Model UserAuthentication( String username, password)

---
+++ UserService/src/main/java/com/sit/cloudnative/UserService/user/UserAuthentication.java
@@ -0,0 +1,17 @@
+package com.sit.cloudnative.UserService.user;
+
+public class UserAuthentication{
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+
+}
