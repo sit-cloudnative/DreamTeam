2126d958385e87237bc56f19019b0e0b476d0e6e
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2018-10-30 10:23:41 +0700

    [UPDATE] GET /user/{studentId} in UserController

--- UserService/src/main/java/com/sit/cloudnative/UserService/user/UserController.java
+++ UserService/src/main/java/com/sit/cloudnative/UserService/user/UserController.java
@@ -5,7 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,13 +22,17 @@ public class UserController {
         String username = userAuthentication.getUsername();
         String password = userAuthentication.getPassword();
         User user_return = userService.findUserByUsernameAndPassword(username, password);
-        return new ResponseEntity<User>(user_return, HttpStatus.OK);
+        return new ResponseEntity<>(user_return, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public ResponseEntity<String> logout() {
         String message = "Logout success...";
-        return new ResponseEntity<String>(message, HttpStatus.OK);
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
+    @RequestMapping(value = "/user/{studentId}", method = RequestMethod.GET)
+    public ResponseEntity<User> getUserByStudentId(@PathVariable long studentId) {
+        return new ResponseEntity<>(userService.findUserByStudentId(studentId),  HttpStatus.OK);
+    }
 }
