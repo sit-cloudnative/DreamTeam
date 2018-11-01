4b918bbd44047441613952d48a1510071a1138b6
Lemon PS <59130500063@mail.kmutt.ac.th>
2018-11-02 00:41:35 +0700

    [ADD] front-end Material

---
+++ nextjs/pages/Material/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../../components/NavBar'
+import Material from '../../components/Material'
+
+export default class index extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <NavBar />
+                <Material />
+            </div>    
+        )
+    }
+}
