02bce9760b44ac593d2c28b0f7b09dbbd29d2065
divinize <patric.emmel@mail.kmutt.ac.th>
2018-11-09 12:04:17 +0700

    [ADD]Add user homepage

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,22 +1,38 @@
 import React from 'react'
+import ProfileCard from '../components/ProfileCard'
+import FavoriteSubjectCard from '../components/FavoriteSubjectCard'
 import axios from '../util/axios'
 
 export default class index extends React.Component{
   constructor(){
     super()
     this.state = {
-      video:{
-        player:{
-          hls_url:''
-        }
+      profile:{
+        studentId:'',
+        firstname:'',
+        lastname:'',
+        favoriteSubject:[
+          {
+            subjectId:0,
+            subjectName:''
+          }
+        ]
       }
     }
+  }
+
+  async componentDidMount() {
+    let user = localStorage.getItem('profileId')
+    let {data} = await axios.get('profile-service/profile/'+user)
+    this.setState({profile:data})
+    console.log(data)
   }
   
   render(){
     return (
       <div className ="container">
-        indexpage
+        <ProfileCard profile={this.state.profile} />
+        <FavoriteSubjectCard favoriteSubjects={this.state.profile.favoriteSubject} />
       </div>
     )
   }
