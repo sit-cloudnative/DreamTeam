15982d8d11c481cf9232a4475b22a30c281b04d0
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-09 18:54:28 +0700

    [UPDATE] frontend fix conflict profile card

--- nextjs/components/ProfileCard.js
+++ nextjs/components/ProfileCard.js
@@ -5,14 +5,8 @@ const ProfileCard = (props) => {
     return (
 
         <div>
-            <link rel="stylesheet" href="/static/bootstrap.min.css" />
-            <link href="/static/style.css" rel="stylesheet" />
-
-
-            <p className='font1'>D-Learning</p>
-            <p className='font2'>{props.profile.firstname + '  ' + props.profile.lastname}<SearchBar handleOnSearch={props.handleOnSearch} />
-             </p>
-
+            This is profile
+            <p className='font2'>{props.profile.firstname + '  ' + props.profile.lastname}</p>
         </div>
     )
 }
