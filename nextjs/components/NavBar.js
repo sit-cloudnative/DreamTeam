4cb9ae35c6c870814c25486b1cd541bf085755f8
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.th>
2018-11-09 22:59:31 +0700

    [UPDATE] navbar; resize&change color icon  + rearrange

--- nextjs/components/NavBar.js
+++ nextjs/components/NavBar.js
@@ -31,28 +31,32 @@ export default class NavBar extends React.Component {
 
   render() {
     return (
-      <div className="">
-        <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-          <a className="navbar-brand" href="#">Dream-Learning <i className="fa fa-cloud" style={{ fontSize: 35, color: 'aquamarine' }}></i></a>
+
+      <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+        <div className="col-md-2">
+          <a className="navbar-brand" href="/"><i className="fa fa-cloud" style={{ fontSize: 18, color: 'white' }}></i> Dream-Learning </a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-
+        </div>
+        <div className="col-md-7">
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
                 <a id="home" className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
               </li>
               <li>
-                <a id="subjects" className="nav-link" href="/subjects">All Curriculum <span className="sr-only">(current)</span></a>
+                <a id="subjects" className="nav-link" href="/subjects">Curriculum <span className="sr-only">(current)</span></a>
               </li>
             </ul>
-            <SearchBar handleOnSearch={this.props.handleOnSearch} />
-              {/* <div>{this.state.profile.studentId} {this.state.profile.firstname}</div> */}
           </div>
-        </nav>
-      </div>
+        </div>
+        <div className="col-md-3">
+          <SearchBar handleOnSearch={this.props.handleOnSearch} />
+        </div>
+      </nav >
+
     )
   }
 }
