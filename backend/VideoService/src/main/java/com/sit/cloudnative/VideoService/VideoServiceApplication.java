27813ec102e49087f63fad4a97dd2965f8f960ba
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-17 16:34:45 +0700

    [ADD] VideoService / init spring-boot project

---
+++ backend/VideoService/src/main/java/com/sit/cloudnative/VideoService/VideoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sit.cloudnative.VideoService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoServiceApplication.class, args);
+	}
+}
