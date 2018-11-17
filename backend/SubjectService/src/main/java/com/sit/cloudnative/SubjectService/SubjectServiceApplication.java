9acb3c9cd2f1b993c9cb960af2ae8792139415de
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-17 16:33:56 +0700

    [ADD] SubjectService / init spring-boot project

---
+++ backend/SubjectService/src/main/java/com/sit/cloudnative/SubjectService/SubjectServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sit.cloudnative.SubjectService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubjectServiceApplication.class, args);
+	}
+}
---
+++ backend/SubjectService/src/test/java/com/sit/cloudnative/SubjectService/SubjectServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sit.cloudnative.SubjectService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SubjectServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
