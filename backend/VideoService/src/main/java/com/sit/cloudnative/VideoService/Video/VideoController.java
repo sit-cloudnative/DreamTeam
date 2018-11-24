d086bd74cd9f41d40426a601bbcdc426e4766cb2
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2018-11-24 14:33:08 +0700

    [UPDATE] Use JWT in VideoController

--- backend/VideoService/src/main/java/com/sit/cloudnative/VideoService/Video/VideoController.java
+++ backend/VideoService/src/main/java/com/sit/cloudnative/VideoService/Video/VideoController.java
@@ -1,11 +1,16 @@
 package com.sit.cloudnative.VideoService.Video;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.sit.cloudnative.VideoService.TokenService;
+import com.sit.cloudnative.VideoService.exception.BadRequestException;
+import com.sit.cloudnative.VideoService.exception.UnauthorizedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -14,10 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class VideoController {
     
     @Autowired
-    VideoService videoService;
+    private VideoService videoService;
+    
+    @Autowired
+    private TokenService tokenService;
     
     @GetMapping("/video/{videoId}")
-    public ResponseEntity<Video> getVideo(@PathVariable long videoId){
+    public ResponseEntity<Video> getVideo(@PathVariable long videoId, @RequestHeader("Authorization") String auth){
+        if(auth.isEmpty()){
+            throw new BadRequestException("Not have value in Authorization");
+        }
+        try {
+            tokenService.checkToken(auth);
+        } catch (JWTVerificationException e) {
+            throw new UnauthorizedException(e.getMessage());
+        }
         return new ResponseEntity<>(videoService.getVideoById(videoId), HttpStatus.OK);
     }
 }
--- backend/VideoService/src/main/java/com/sit/cloudnative/VideoService/Videos/VideosController.java
+++ backend/VideoService/src/main/java/com/sit/cloudnative/VideoService/Videos/VideosController.java
@@ -1,5 +1,9 @@
 package com.sit.cloudnative.VideoService.Videos;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.sit.cloudnative.VideoService.TokenService;
+import com.sit.cloudnative.VideoService.exception.BadRequestException;
+import com.sit.cloudnative.VideoService.exception.UnauthorizedException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -7,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +20,20 @@ public class VideosController {
 
     @Autowired
     private VideosService videosService;
+    
+    @Autowired
+    private TokenService tokenService;
 
     @GetMapping("/videos/{subjectId}")
-    public ResponseEntity<List<Videos>> getVideoList(@PathVariable long subjectId) {
+    public ResponseEntity<List<Videos>> getVideoList(@PathVariable long subjectId, @RequestHeader("Authorization") String auth) {
+        if(auth.isEmpty()){
+            throw new BadRequestException("Not have value in Authorization");
+        }
+        try {
+            tokenService.checkToken(auth);
+        } catch (JWTVerificationException e) {
+            throw new UnauthorizedException(e.getMessage());
+        }
         List<Videos> videoList = videosService.getVideoListBySubjectId(subjectId);
         return new ResponseEntity<>(videoList, HttpStatus.OK);
     }
