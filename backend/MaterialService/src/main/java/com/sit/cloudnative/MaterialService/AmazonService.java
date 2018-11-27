e627a127391b25433563cccf39c55cbbbfed1e83
rattanan.nan@mail.kmutt.ac.th <rattanan.nan@mail.kmutt.ac.th>
2018-11-27 14:46:35 +0700

    [UPDATE] Material can upload listFiles download delete to aws s3 directly

--- backend/MaterialService/src/main/java/com/sit/cloudnative/MaterialService/AmazonService.java
+++ backend/MaterialService/src/main/java/com/sit/cloudnative/MaterialService/AmazonService.java
@@ -1,87 +1,63 @@
 package com.sit.cloudnative.MaterialService;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import javax.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class AmazonService {
-    
+
     private AmazonS3 s3client;
-    
+
     @Value("${amazon.bucketName}")
     private String bucketName;
-    
+
     @Value("${amazon.accessKey}")
     private String accessKey;
-    
+
     @Value("${amazon.secretKey}")
     private String secretKey;
-    
+
     @Value("${amazon.regions}")
     private String region;
 
-    @Value("${AMAZON_ENDPOINTURL}")
+    @Value("${amazon.endpointUrl}")
     private String endpointUrl;
 
+    Logger logger = LoggerFactory.getLogger(MaterialController.class);
+    
     public AmazonService() {
     }
-    
+
     @PostConstruct
     private void initializeAmazon() {
-       AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
-       this.s3client = AmazonS3ClientBuilder
-               .standard()
-               .withCredentials(new AWSStaticCredentialsProvider(credentials))
-               .withRegion(region)
-               .build();
-    }
-      
-//    public void uploadFile() {
-//        try {
-//            TransferManager tm = TransferManagerBuilder.standard()
-//                                .withS3Client(s3client)
-//                                .build();
-//            Upload upload = tm.upload(bucketName, "test7.txt", new File("C:\\Users\\sangu\\Documents\\test.txt"));
-//        }
-//        catch(AmazonServiceException e) {
-//            // The call was transmitted successfully, but Amazon S3 couldn't process
-//            // it, so it returned an error response.
-//            e.printStackTrace();
-//        }
-//        catch(SdkClientException e) {
-//            // Amazon S3 couldn't be contacted for a response, or the client
-//            // couldn't parse the response from Amazon S3.
-//            e.printStackTrace();
-//        }
-//    }
-
-    private File convertMultiPartToFile(MultipartFile file) throws IOException {
-        File convFile = new File(file.getOriginalFilename());
-        FileOutputStream fos = new FileOutputStream(convFile);
-        fos.write(file.getBytes());
-        fos.close();
-        return convFile;
-    }
-
-    private String generateFileName(MultipartFile multiPart) {
-        return new Date().getTime() + "-" + multiPart.getOriginalFilename().replace(" ", "_");
-    }
-
-    private void uploadFileTos3bucket(String fileName, File file) {
-        s3client.putObject(new PutObjectRequest(bucketName, fileName, file)
-                .withCannedAcl(CannedAccessControlList.PublicRead));
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        this.s3client = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
     }
 
     public String uploadFile(MultipartFile multipartFile) {
@@ -101,12 +77,78 @@ public class AmazonService {
     public String deleteFileFromS3Bucket(String fileName) {
         String deleteStatus = "";
         try {
-            s3client.deleteObject(new DeleteObjectRequest(bucketName + "/", fileName));
-            deleteStatus =  "Delete File [" + fileName + "] Successfully!";
+            s3client.deleteObject(new DeleteObjectRequest(bucketName, fileName));
+            deleteStatus = "Delete File [" + fileName + "] Successfully!";
         } catch (Exception e) {
             e.printStackTrace();
         }
         return deleteStatus;
+    }
+
+    public List<String> listFiles() {
+        ListObjectsRequest listObjectsRequest = new ListObjectsRequest()
+                .withBucketName(bucketName);
+        List<String> keys = new ArrayList<>();
+        ObjectListing objects = s3client.listObjects(listObjectsRequest);
+        while (true) {
+            List<S3ObjectSummary> summaries = objects.getObjectSummaries();
+            if (summaries.size() < 1) {
+                break;
+            }
+            for (S3ObjectSummary item : summaries) {
+                if (!item.getKey().endsWith("/")) {
+                    keys.add(item.getKey());
+                }
+            }
+            objects = s3client.listNextBatchOfObjects(objects);
+        }
+        return keys;
+    }
+
+    public ByteArrayOutputStream downloadFile(String keyName) {
+        try {
+            S3Object s3object = s3client.getObject(new GetObjectRequest(bucketName, keyName));
+
+            InputStream is = s3object.getObjectContent();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int len;
+            byte[] buffer = new byte[4096];
+            while ((len = is.read(buffer, 0, buffer.length)) != -1) {
+                baos.write(buffer, 0, len);
+            }
+            return baos;
+        } catch (IOException ioe) {
+            logger.error("IOException: " + ioe.getMessage());
+        } catch (AmazonServiceException ase) {
+            logger.info("sCaught an AmazonServiceException from GET requests, rejected reasons:");
+            logger.info("Error Message:    " + ase.getMessage());
+            logger.info("HTTP Status Code: " + ase.getStatusCode());
+            logger.info("AWS Error Code:   " + ase.getErrorCode());
+            logger.info("Error Type:       " + ase.getErrorType());
+            logger.info("Request ID:       " + ase.getRequestId());
+            throw ase;
+        } catch (AmazonClientException ace) {
+            logger.info("Caught an AmazonClientException: ");
+            logger.info("Error Message: " + ace.getMessage());
+            throw ace;
+        }
+        return null;
+    }
+
+    private File convertMultiPartToFile(MultipartFile file) throws IOException {
+        File convFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
+    }
+
+    private String generateFileName(MultipartFile multiPart) {
+        return new Date().getTime() + "-" + multiPart.getOriginalFilename().replaceAll("[^.,a-zA-Z0-9]", "_");
+    }
+
+    private void uploadFileTos3bucket(String fileName, File file) {
+        s3client.putObject(new PutObjectRequest(bucketName, fileName, file));
     }
 
 }
--- backend/MaterialService/src/main/java/com/sit/cloudnative/MaterialService/MaterialController.java
+++ backend/MaterialService/src/main/java/com/sit/cloudnative/MaterialService/MaterialController.java
@@ -1,28 +1,65 @@
 package com.sit.cloudnative.MaterialService;
 
+import java.io.ByteArrayOutputStream;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class MaterialController {
-    
+
     @Autowired
     private AmazonService amazonService;
 
-//    @GetMapping("/q")
-//    public String daaqqq() {
-//        amazonService.uploadFile();
-//        return "qqqq";
-//    }
-
-    @PostMapping("/uploadFile")
-    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
-        return this.amazonService.uploadFile(file);
+    @PostMapping("/file")
+    public ResponseEntity<String> uploadFile(@RequestPart(value = "file") MultipartFile file) {
+        return new ResponseEntity<String>(amazonService.uploadFile(file), HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/deleteFile")
-    public String deleteFile(@RequestPart(value = "fileName") String fileName) {
-        return this.amazonService.deleteFileFromS3Bucket(fileName);
+    @GetMapping("/files")
+    public ResponseEntity<List<String>> listAllFiles() {
+        return new ResponseEntity<List<String>>(amazonService.listFiles(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/file")
+    public ResponseEntity<String> deleteFile(@RequestPart(value = "fileName") String fileName) {
+        return new ResponseEntity<String>(amazonService.deleteFileFromS3Bucket(fileName), HttpStatus.OK);
+    }
+
+    @GetMapping("/file/{keyname}")
+    public ResponseEntity<byte[]> downloadFile(@PathVariable String keyname) {
+        ByteArrayOutputStream downloadInputStream = amazonService.downloadFile(keyname);
+
+        return ResponseEntity.ok()
+                .contentType(contentType(keyname))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + keyname + "\"")
+                .body(downloadInputStream.toByteArray());
+    }
+
+    private MediaType contentType(String keyname) {
+        String[] arr = keyname.split("\\.");
+        String type = arr[arr.length - 1];
+        switch (type) {
+            case "txt":
+                return MediaType.TEXT_PLAIN;
+            case "png":
+                return MediaType.IMAGE_PNG;
+            case "jpg":
+                return MediaType.IMAGE_JPEG;
+            case "pdf":
+                return MediaType.APPLICATION_PDF;
+            default:
+                return MediaType.APPLICATION_OCTET_STREAM;
+        }
     }
 }
