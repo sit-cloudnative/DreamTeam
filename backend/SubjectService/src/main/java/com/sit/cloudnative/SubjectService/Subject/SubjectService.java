45ec17752c1eda3966c73abae3c7c20752b146a0
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-28 13:41:08 +0700

    [UPDATE] SubjectService / remove try catch from service

--- backend/SubjectService/src/main/java/com/sit/cloudnative/SubjectService/Subject/SubjectService.java
+++ backend/SubjectService/src/main/java/com/sit/cloudnative/SubjectService/Subject/SubjectService.java
@@ -2,14 +2,12 @@ package com.sit.cloudnative.SubjectService.Subject;
 
 import com.sit.cloudnative.SubjectService.Curriculum.Curriculum;
 import com.sit.cloudnative.SubjectService.Curriculum.CurriculumService;
-import com.sit.cloudnative.SubjectService.exception.NotFoundException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 
 @Service
 public class SubjectService {
@@ -21,22 +19,12 @@ public class SubjectService {
     private CurriculumService curriculumService;
 
     public List<Subject> getSubjectListByCurriculumId(long curriculumId) {
-        List<Subject> subjectList = new ArrayList<Subject>();
-        try{
-            subjectAdapter.getSubjectListByCurriculumId(curriculumId);
-        }catch(HttpClientErrorException httpException){
-            throw new NotFoundException("target curriculumn id \'" + curriculumId + "\' is not found");
-        }
+        List<Subject> subjectList = subjectAdapter.getSubjectListByCurriculumId(curriculumId);
         return subjectList;
     }
 
     public Subject getSubjectById(long subjectId) {
-        Subject subject = new Subject();
-        try{
-            subject = subjectAdapter.getSubjectBySubjectId(subjectId);
-        }catch(HttpClientErrorException httpException){
-            throw new NotFoundException("target subject id \'" + subjectId + "\' is not found");
-        }
+        Subject subject = subjectAdapter.getSubjectBySubjectId(subjectId);
         return subject;
     }
 
