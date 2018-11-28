aa3a2b5e38fbf5e71dea45682ed7f0da802e82a3
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2018-11-28 16:20:12 +0700

    [FIX] remove comments in MaterialRepository

--- backend/MaterialService/src/main/java/com/sit/cloudnative/MaterialService/MaterialRepository.java
+++ backend/MaterialService/src/main/java/com/sit/cloudnative/MaterialService/MaterialRepository.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.sit.cloudnative.MaterialService;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MaterialRepository extends JpaRepository<Material, Long>{
-    
+public interface MaterialRepository extends JpaRepository<Material, Long> {
+
 }
