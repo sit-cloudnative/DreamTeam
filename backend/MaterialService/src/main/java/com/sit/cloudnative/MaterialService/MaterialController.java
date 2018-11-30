909545911d06ee38713514036a6ea59ce8a1dec7
rattanan.nan@mail.kmutt.ac.th <rattanan.nan@mail.kmutt.ac.th>
2018-11-30 23:16:03 +0700

    [ADD] CrossOrigin in MaterialController

--- backend/MaterialService/src/main/java/com/sit/cloudnative/MaterialService/MaterialController.java
+++ backend/MaterialService/src/main/java/com/sit/cloudnative/MaterialService/MaterialController.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class MaterialController {
 
     @Autowired
