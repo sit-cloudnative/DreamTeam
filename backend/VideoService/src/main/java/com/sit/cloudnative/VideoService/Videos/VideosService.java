2290fd134383e69aeef8b69a38960aa3719d3dc7
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-30 18:15:38 +0700

    [UPDATE] VideoService / orderVideo to desc

--- backend/VideoService/src/main/java/com/sit/cloudnative/VideoService/Videos/VideosService.java
+++ backend/VideoService/src/main/java/com/sit/cloudnative/VideoService/Videos/VideosService.java
@@ -1,6 +1,8 @@
 package com.sit.cloudnative.VideoService.Videos;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,21 @@ public class VideosService {
     private VideosAdapter videosAdapter;
 
     public List<Videos> getVideoListBySubjectId(long subjectId) {
-        return Arrays.asList(videosAdapter.getVideo(subjectId));
+        List<Videos> videos = Arrays.asList(videosAdapter.getVideo(subjectId));
+        orderVideoByVideoIdDescending(videos);
+        return videos;
+    }
+
+    private void orderVideoByVideoIdDescending(List<Videos> videos) {
+        Collections.sort(videos, new Comparator<Videos>() {
+            @Override
+            public int compare(Videos first, Videos second) {
+                if (first.getVideoId() > second.getVideoId())
+                    return -1;
+                if (first.getVideoId() < second.getVideoId())
+                    return 1;
+                return 0;
+            }
+        });
     }
 }
