9ee9305b5111d0d37c96f705a528b6994a97087d
rattanan.nan@mail.kmutt.ac.th <rattanan.nan@mail.kmutt.ac.th>
2018-11-30 23:10:16 +0700

    [UPDATE] JWT, Exception, Logger in MaterialService

--- backend/MaterialService/src/main/java/com/sit/cloudnative/MaterialService/MaterialController.java
+++ backend/MaterialService/src/main/java/com/sit/cloudnative/MaterialService/MaterialController.java
@@ -6,6 +6,9 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 
 import com.sit.cloudnative.MaterialService.exception.BadRequestException;
+import com.sit.cloudnative.MaterialService.exception.NotFoundException;
+import com.sit.cloudnative.MaterialService.exception.UnauthorizedException;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -16,52 +19,88 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class MaterialController {
-    
+
     @Autowired
     private MaterialService materialService;
-    
+
+    @Autowired
+    private TokenService tokenService;
+
     Logger logger = LoggerFactory.getLogger(MaterialController.class);
-    
+
     @GetMapping("/files")
-    public ResponseEntity<List<Material>> getAllFiles() {
+    public ResponseEntity<List<Material>> getAllFiles(@RequestHeader("Authorization") String auth, HttpServletRequest request) {
+        tokenService.validateToken(auth, request, logger);
         return new ResponseEntity<List<Material>>(materialService.getMaterialList(), HttpStatus.OK);
     }
-    
+
     @GetMapping("/files/{subjectCode}")
-    public ResponseEntity<List<Material>> getFilesBySubjectCode(@PathVariable String subjectCode) {
+    public ResponseEntity<List<Material>> getFilesBySubjectCode(@PathVariable String subjectCode, @RequestHeader("Authorization") String auth, HttpServletRequest request) {
+        tokenService.validateToken(auth, request, logger);
         return new ResponseEntity<List<Material>>(materialService.getMaterialListBySubjectCode(subjectCode), HttpStatus.OK);
     }
-    
+
     @PostMapping("/file/{subjectCode}")
-    public ResponseEntity<Material> uploadFile(@PathVariable String subjectCode, @RequestPart(value = "file") MultipartFile file) {
+    public ResponseEntity<Material> uploadFile(@PathVariable String subjectCode, @RequestPart(value = "file") MultipartFile file, @RequestHeader("Authorization") String auth, HttpServletRequest request) {
+        tokenService.validateToken(auth, request, logger);
+        String username = tokenService.getUser(auth);
+        if (!"admin".equals(tokenService.getRole(auth))) {
+            logger.warn(System.currentTimeMillis() + " | " + username + " | " + "try to upload file");
+            throw new UnauthorizedException("Not have permission");
+        }
         try {
-            Material material = materialService.uploadMaterial(subjectCode, file);
+            Material material = materialService.uploadMaterial(subjectCode, file, username);
+            logger.info(System.currentTimeMillis() + " | " + username + " | " + "upload material name (" + material.getFileName() + ")");
             return new ResponseEntity<Material>(material, HttpStatus.CREATED);
         } catch (IOException ex) {
-            throw new BadRequestException();
+            logger.warn(System.currentTimeMillis() + " | " + username + " | " + "cannot upload file IOException");
+            throw new BadRequestException("Cannot upload this time");
         }
     }
-    
+
     @DeleteMapping("/file/{materialId}")
-    public ResponseEntity<Long> deleteFile(@PathVariable long materialId) {
-        return new ResponseEntity<Long>(materialService.deleteMaterialById(materialId), HttpStatus.OK);
+    public ResponseEntity<Long> deleteFile(@PathVariable long materialId, @RequestHeader("Authorization") String auth, HttpServletRequest request) {
+        tokenService.validateToken(auth, request, logger);
+        String username = tokenService.getUser(auth);
+        if (!"admin".equals(tokenService.getRole(auth))) {
+            logger.warn(System.currentTimeMillis() + " | " + username + " | " + "try to delete material id (" + materialId + ")");
+            throw new UnauthorizedException("Not have permission");
+        }
+        try {
+            long deleteId = materialService.deleteMaterialById(materialId);
+            logger.info(System.currentTimeMillis() + " | " + username + " | " + "delete material id (" + deleteId + ")");
+            return new ResponseEntity<Long>(deleteId, HttpStatus.OK);
+        } catch (HttpClientErrorException e) {
+            logger.warn(System.currentTimeMillis() + " | " + username + " | " + "not found material id (" + materialId + ")");
+            throw new NotFoundException(e.getMessage());
+        }
     }
-    
+
     @GetMapping("/file/{materialId}")
-    public ResponseEntity<byte[]> downloadFile(@PathVariable long materialId) {
-        Material material = materialService.getMaterialById(materialId).get();
-        return ResponseEntity.ok()
-                .contentType(contentType(material.getFileName()))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + material.getFileName() + "\"")
-                .body(materialService.downloadMaterial(material));
+    public ResponseEntity<byte[]> downloadFile(@PathVariable long materialId, @RequestHeader("Authorization") String auth, HttpServletRequest request) {
+        tokenService.validateToken(auth, request, logger);
+        String username = tokenService.getUser(auth);
+        try {
+            Material material = materialService.getMaterialById(materialId).get();
+            logger.info(System.currentTimeMillis() + " | " + username + " | " + "download material id (" + materialId + ")");
+            return ResponseEntity.ok()
+                    .contentType(contentType(material.getFileName()))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + material.getFileName() + "\"")
+                    .body(materialService.downloadMaterial(material));
+        } catch (HttpClientErrorException e) {
+            logger.warn(System.currentTimeMillis() + " | " + username + " | " + "not found material id (" + materialId + ")");
+            throw new NotFoundException(e.getMessage());
+        }
     }
-    
+
     private MediaType contentType(String keyname) {
         String[] arr = keyname.split("\\.");
         String type = arr[arr.length - 1];
