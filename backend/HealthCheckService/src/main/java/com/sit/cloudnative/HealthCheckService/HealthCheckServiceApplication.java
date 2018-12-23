12161dfb6f17c855062fb9e7c6bd90185423c207
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.th>
2018-12-23 18:03:09 +0700

    init HealthCheck service

---
+++ backend/HealthCheckService/src/main/java/com/sit/cloudnative/HealthCheckService/HealthCheckServiceApplication.java
@@ -0,0 +1,14 @@
+package com.sit.cloudnative.HealthCheckService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCheckServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCheckServiceApplication.class, args);
+	}
+
+}
+
---
+++ backend/HealthCheckService/src/test/java/com/sit/cloudnative/HealthCheckService/HealthCheckServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sit.cloudnative.HealthCheckService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HealthCheckServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
